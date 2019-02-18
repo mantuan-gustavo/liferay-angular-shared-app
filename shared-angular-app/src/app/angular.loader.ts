f84068960d3fa7a499c43a6281739212f7ce5572
Douglas Gatti <douglas.gatti@liferay.com>
2019-02-18 10:14:20 -0300

    Upgrade to angular 7.2 and create angular loader function

---
+++ shared-angular-app/src/app/angular.loader.ts
@@ -0,0 +1,15 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+import { AppLoader } from './app.loader';
+import { AppModule } from './app.module';
+
+export function loadApp(namespace: string) {
+	platformBrowserDynamic()
+		.bootstrapModule(AppModule)
+		.then((injector: any) => {
+			new AppLoader(injector).loadComponent(AppComponent, namespace);
+
+      console.log("BOOTSTRAPING-APP[", namespace, "]");
+		});
+};
--- shared-angular-app/src/main.ts
+++ shared-angular-app/src/main.ts
@@ -1,10 +1,12 @@
 import { enableProdMode } from '@angular/core';
+import 'zone.js';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { AppComponent } from './app/app.component';
 import { AppLoader } from './app/app.loader';
 import { environment } from './environments/environment';
+import { loadApp } from './app/angular.loader';
 
 if (environment.production) {
   enableProdMode();
@@ -16,11 +18,7 @@ if (appElements.length > 0) {
   for (let i = 0; i < appElements.length; i++) {
 
     if (appElements[i].children.length === 0) {
-      platformBrowserDynamic().bootstrapModule(AppModule).then(
-        (injector: any) => {
-          new AppLoader(injector).loadComponent(AppComponent, appElements[i].id);
-          console.log("BOOTSTRAPING-APP[", appElements[i].id, "]");
-        });
+      loadApp(appElements[i].id);
     }
   };
 }
